8629a41339defe907640f34970a3ee562023ef42
Alban Nanty <banban@banban.org>
2010-04-11 04:12:23 +0800

    try to display at least something

--- dir_list.php
+++ dir_list.php
@@ -1,13 +1,15 @@
 <?php
-    header('Content-Type: text/plain') ;
-	$dir = "parts";
+    header('Content-Type: text/plain');
+	$dir = "./parts";
 	$dh = opendir($dir);
+	echo $dh;
+	echo "---\r\n";
 	while (($file = readdir($dh)) !== false)
 	{
 //		if (is_dir($dir.'/'.$file) && $file != "." && $file != "..")
 		{
-            echo $file."\r\n" ;
+            echo $file."\r\n";
 		}
 	}   
-    closedir($dh) ;
+    closedir($dh);
 ?>
