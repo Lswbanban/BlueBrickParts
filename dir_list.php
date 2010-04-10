a14187c03e594d95d0f9e953948c4578844f260f
Alban Nanty <banban@banban.org>
2010-04-11 04:06:39 +0800

    | test something more simple

--- dir_list.php
+++ dir_list.php
@@ -6,7 +6,7 @@
 	$dh = opendir($dir);
 	while (($file = readdir($dh)) !== false)
 	{
-		if (is_dir($dir.'/'.$file) && $file != "." && $file != "..")
+//		if (is_dir($dir.'/'.$file) && $file != "." && $file != "..")
 		{
             echo $file."\r\n" ;
 		}
