ced433f0699bd28eaddbbe0574af4fe26ce73c6f
Alban Nanty <banban@banban.org>
2010-04-11 04:00:38 +0800

    | fix some php bug in the dir_list file

--- dir_list.php
+++ dir_list.php
@@ -2,15 +2,16 @@
 <HEAD>
 <?php
     header('Content-Type: text/plain') ;
-    $dir = opendir('parts') ;
-    while ( ($entry = readdir($dir)) !== FALSE )
-    {
-        if ( is_file($entry) )
-        {
-            echo $entry, "\r\n" ;
-        }
-    }
-    closedir($dp) ;
+	$dir = "parts";
+	$dh = opendir($dir);
+	while (($file = readdir($dh)) !== false)
+	{
+		if (is_dir($dir.'/'.$file) && $file != "." && $file != "..")
+		{
+            echo $file."\r\n" ;
+		}
+	}   
+    closedir($dh) ;
 ?>
 </HEAD>
 </HTML>
