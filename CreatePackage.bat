0454e60983e27d05cc66e4fa1c068349e2596455
Alban Nanty <banban@banban.org>
2010-04-28 00:24:50 +0800

    + add a batch file to create automatically the zip file for different part package

---
+++ CreatePackage.bat
@@ -0,0 +1,6 @@
+REM this batch file is used to create all the zip files for all the different packages of part we may distribute on the BlueBrick website
+REM This batch iterate through all the text files that it can find in the PackageDefinition folder
+
+for %%I in (PackageDefinition\*) do ( "C:\Program Files\WinRAR\WinRAR.exe" a %%~nI.zip @%%I )
+
+pause
