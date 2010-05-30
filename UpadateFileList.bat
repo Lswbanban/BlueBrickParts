fa8e0dbbd8f7f3825589f11ebcfb83d5c20543ac
Alban Nanty <banban@banban.org>
2010-05-30 19:46:55 +0800

    | update the batch file to create the file list, in order to create the list in alphabetic order. | update the folder list to keep the alphabetic order.

--- UpadateFileList.bat
+++ UpadateFileList.bat
@@ -1,5 +1,5 @@
 REM updat the folder list
-dir parts\* /B /AD > parts\_folderlist.txt
+dir parts\* /B /ON /AD > parts\_folderlist.txt
 
 REM update the part list
 for /d %%D in (parts\*) do (dir %%D\*.gif %%D\*.xml /B /ON > %%D\_partlist.txt)
