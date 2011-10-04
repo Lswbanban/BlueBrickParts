fbaf8668395218442fce5c98d04790f40575942c
Alban Nanty <banban@banban.org>
2011-10-05 02:29:07 +0800

    | fix the batch file that generate the file list to support space in the path name | then update the part list for "Train Set"

--- UpadateFileList.bat
+++ UpadateFileList.bat
@@ -2,4 +2,4 @@ REM updat the folder list
 dir parts\* /B /ON /AD > parts\_folderlist.txt
 
 REM update the part list
-for /d %%D in (parts\*) do (dir %%D\*.gif %%D\*.xml /B /ON > %%D\_partlist.txt)
+for /d %%D in (parts\*) do (dir "%%D\*.gif" "%%D\*.xml" /B /ON > "%%D\_partlist.txt")
