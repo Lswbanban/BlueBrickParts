51d3f76d257cc2bbdcd2b2f657f26f3e18a318cf
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2018-02-12 20:26:49 +0800

    | forgot to commit the deletion of the old file

--- UpadateFileList.bat
+++
@@ -1,5 +0,0 @@
-REM updat the folder list
-dir parts\* /B /ON /AD > parts\_folderlist.txt
-
-REM update the part list
-for /d %%D in (parts\*) do (dir "%%D\*.gif" "%%D\*.xml" /B /ON > "%%D\_partlist.txt")
