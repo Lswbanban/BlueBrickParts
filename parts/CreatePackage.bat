b2d82fe3fea738e4d7c8c8659530d9055f24796e
Alban Nanty <banban@banban.org>
2021-01-17 13:45:22 +0800

    | move the create package bat file in the parts folder in order to remove the "parts" folder from the zip | rename the package definition to include their version number according to the new way of doing packages | update the package definition to include the config folder | remove the AllAdditionalParts package, as now user can download easily package from inside BlueBrick so this convenient zip can be removed from the website

--- parts/CreatePackage.bat
+++ parts/CreatePackage.bat
@@ -1,6 +1,6 @@
 REM this batch file is used to create all the zip files for all the different packages of part we may distribute on the BlueBrick website
 REM This batch iterate through all the text files that it can find in the PackageDefinition folder
 
-for %%I in (PackageDefinition\*) do ( "D:\Program Files (x86)\WinRAR\WinRAR.exe" a %%~nI.zip @%%I )
+for %%I in (..\PackageDefinition\*) do ( "D:\Program Files (x86)\WinRAR\WinRAR.exe" a %%~nI.zip @%%I )
 
 IF NOT ERRORLEVEL 0 pause
